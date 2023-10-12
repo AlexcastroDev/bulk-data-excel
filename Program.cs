81cddc1b78277f0705caaac38a7c32b4395f4bf5
Alex Oliveira <alexoliveira7x@gmail.com>
2023-10-12 17:17:26 +0100

    start project

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using ExcelDataReader;
+using System.Text;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+using (var stream = File.OpenRead("RELATORIO_DTB_BRASIL_MUNICIPIO.xlsx"))
+{
+    using var reader = ExcelReaderFactory.CreateReader(stream);
+    reader.Read();
+
+    var headers = new List<string>();
+
+    for (int i = 0; i < reader.FieldCount; i++)
+    {
+        headers.Add(reader.GetString(i));
+    }
+    if (headers[0] != "ID" || headers[1] != "State_Code" || headers[2] != "State_Name")
+    {
+        throw new Exception("Invalid headers");
+    }
+
+    do
+    {
+        while (reader.Read())
+        {
+            try
+            {
+                var id = reader.GetValue(0);
+                var stateCode = reader.GetValue(1);
+                var stateName = reader.GetValue(2);
+                Console.WriteLine($"{id} - {stateCode} - {stateName}");
+            }
+            catch
+            {
+                // Maybe write a column with error (?)
+            }
+        }
+    } while (reader.NextResult());
+}
